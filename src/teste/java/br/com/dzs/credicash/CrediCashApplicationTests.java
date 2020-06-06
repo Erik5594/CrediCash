87a5243ad8ff951d3eda291835f029d4d60c3b7c
erik-qo <erik.derick74@gmail.com>
2020-06-05 22:42:30 -0300

    Criação do projeto CrediCash

--- src/teste/java/br/com/dzs/credicash/CrediCashApplicationTests.java
+++ src/teste/java/br/com/dzs/credicash/CrediCashApplicationTests.java
@@ -1,10 +1,24 @@
 package java.br.com.dzs.credicash;
 
+import br.com.dzs.credicash.CrediCashApplication;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
 /**
  * DZS
  *
  * @author erik_
  * Data Criacao: 05/06/2020 - 22:31
  */
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = CrediCashApplication.class)
+@WebAppConfiguration
 public class CrediCashApplicationTests {
+    @Test
+    public void contextLoads() {
+    }
 }
