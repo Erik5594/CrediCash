7740fd5daa2ce4afe463d4fcc8c89708a768e41d
erik-qo <erik.derick74@gmail.com>
2020-06-06 00:14:14 -0300

    Criação do projeto CrediCash

--- src/main/java/br/com/dzs/credicash/domain/Usuario.java
+++ src/main/java/br/com/dzs/credicash/domain/Usuario.java
@@ -3,15 +3,23 @@ package br.com.dzs.credicash.domain;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 /**
  * DZS
  *
  * @author erik_
  * Data Criacao: 05/06/2020 - 20:35
  */
+@Entity
 public @Data class Usuario {
 
     @JsonInclude(JsonInclude.Include.NON_NULL)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @JsonInclude(JsonInclude.Include.NON_NULL)
